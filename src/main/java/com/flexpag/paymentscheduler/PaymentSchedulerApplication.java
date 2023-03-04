f1603e0ef9455bc9c485326e7764934391e2077a
alecxandy <alecxandy@gmail.com>
2023-03-04 17:01:00 -0300

    Inserindo alguns dados para teste

--- src/main/java/com/flexpag/paymentscheduler/PaymentSchedulerApplication.java
+++ src/main/java/com/flexpag/paymentscheduler/PaymentSchedulerApplication.java
@@ -1,13 +1,33 @@
 package com.flexpag.paymentscheduler;
 
+import com.flexpag.paymentscheduler.enums.Status;
+import com.flexpag.paymentscheduler.model.Payment;
+import com.flexpag.paymentscheduler.repository.PaymentRepository;
+import com.flexpag.paymentscheduler.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
-public class PaymentSchedulerApplication {
+public class PaymentSchedulerApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentSchedulerApplication.class, args);
-	}
+    @Autowired
+    private PaymentRepository paymentRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSchedulerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        paymentRepository.save(new Payment(1L, 10.00, LocalDateTime.parse("2023-01-01T01:30:26"), Status.PAID));
+        paymentRepository.save(new Payment(2L, 10.00, LocalDateTime.parse("2023-02-01T01:30:26"), Status.PAID));
+        paymentRepository.save(new Payment(3L, 10.00, LocalDateTime.parse("2023-03-01T01:30:26"), Status.PAID));
+        paymentRepository.save(new Payment(4L, 10.00, LocalDateTime.parse("2023-04-01T01:30:26"), Status.PENDING));
+        paymentRepository.save(new Payment(5L, 10.00, LocalDateTime.parse("2023-05-01T01:30:26"), Status.PENDING));
+
+    }
 }
