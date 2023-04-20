b06816221b49296df54ce0d88a8a3af44ff02dd1
alecxandy <alecxandy@gmail.com>
2023-04-20 01:38:11 -0300

    Adicionando teste PaymentService

--- src/test/java/com/flexpag/paymentscheduler/service/PaymentServiceTest.java
+++ src/test/java/com/flexpag/paymentscheduler/service/PaymentServiceTest.java
@@ -2,6 +2,7 @@ package com.flexpag.paymentscheduler.service;
 
 import com.flexpag.paymentscheduler.entity.Payment;
 import com.flexpag.paymentscheduler.enums.Status;
+import com.flexpag.paymentscheduler.exception.IdentifierNotFoundException;
 import com.flexpag.paymentscheduler.exception.InvalidDateTimeException;
 import com.flexpag.paymentscheduler.repository.PaymentRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -9,16 +10,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class PaymentServiceTest {
 
+    Page<Payment> PAGE = Page.empty();
+    Pageable PAGEABLE = PageRequest.of(2, 10);
     Payment PAYMENT = new Payment(1L, 10.00, LocalDateTime.of(2024, Month.APRIL, 23, 14, 0, 0), Status.PENDING);
     Payment PAYMENT_PAID = new Payment(1L, 10.00, LocalDateTime.of(2022, Month.APRIL, 23, 14, 0, 0), Status.PAID);
 
@@ -57,18 +67,67 @@ class PaymentServiceTest {
 
     @Test
     void findAll() {
+        when(paymentRepository.findAll(PAGEABLE)).thenReturn(PAGE);
+        Page<Payment> page = paymentService.findAll(PAGEABLE);
+        assertNotNull(page);
+        assertEquals(page.getTotalPages(), 1);
+        assertEquals(page.getTotalElements(), 0L);
     }
 
     @Test
     void status() {
+        when(paymentRepository.findById(1L)).thenReturn(Optional.of(PAYMENT));
+        Status status = paymentService.status(1L);
+        assertNotNull(status);
+        assertEquals(status, Status.PENDING);
+    }
+
+    @Test
+    void notStatus() {
+        when(paymentRepository.findById(Mockito.anyLong()))
+                .thenThrow(new IdentifierNotFoundException("Identifier does not exist"));
+        try {
+            paymentService.status(Mockito.anyLong());
+        } catch (Exception ex) {
+            assertEquals(IdentifierNotFoundException.class, ex.getClass());
+            assertEquals("Identifier does not exist", ex.getMessage());
+        }
     }
 
     @Test
     void findById() {
+        when(paymentRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(PAYMENT));
+        Payment payment = paymentService.findById(Mockito.anyLong());
+        assertNotNull(payment);
+        assertEquals(payment.getClass(), Payment.class);
+        assertEquals(payment.getId(), PAYMENT.getId());
+        assertEquals(payment.getDataTime(), PAYMENT.getDataTime());
+        assertEquals(payment.getPaymentValue(), PAYMENT.getPaymentValue());
+    }
+
+    @Test
+    void notFindById() {
+        when(paymentRepository.findById(Mockito.anyLong()))
+                .thenThrow(new IdentifierNotFoundException("Identifier does not exist"));
+        try {
+            paymentService.findById(Mockito.anyLong());
+        } catch (Exception ex) {
+            assertEquals(ex.getClass(), IdentifierNotFoundException.class);
+            assertEquals(ex.getMessage(), "Identifier does not exist");
+        }
     }
 
     @Test
     void deleteById() {
+        when(paymentRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(PAYMENT));
+        doNothing().when(paymentRepository).deleteById(Mockito.anyLong());
+        paymentService.deleteById(Mockito.anyLong());
+        Mockito.verify(paymentRepository, times(1)).deleteById(Mockito.anyLong());
+    }
+
+    @Test
+    void notDeleteById(){
+
     }
 
     @Test
