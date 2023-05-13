429286ffd1737555832be09d07916b736a1a329a
alecxandy <alecxandy@gmail.com>
2023-05-13 10:43:56 -0300

    Criando repositorio

--- src/main/java/com/flexpag/paymentscheduler/service/UserDetailsService.java
+++ src/main/java/com/flexpag/paymentscheduler/service/UserDetailsService.java
@@ -2,8 +2,12 @@ package com.flexpag.paymentscheduler.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
